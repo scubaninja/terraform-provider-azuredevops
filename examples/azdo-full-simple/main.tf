63a0284a899dfc8b9db336ff21a572a57cd0ddb5
April Edwards <apedward@microsoft.com>
2020-10-15 15:15:36 +0100

    update main.tf for demo

--- examples/azdo-full-simple/main.tf
+++ examples/azdo-full-simple/main.tf
@@ -1,9 +1,9 @@
-resource "azuredevops_project" "p" {
+resource "azuredevops_project" "a" {
   project_name = "ADO Demo Project"
 }
  
 resource "azuredevops_variable_group" "vars" {
-  project_id   = azuredevops_project.p.id
+  project_id   = azuredevops_project.a.id
   name         = "Variable Group - Infra"
   description  = "Managed by Terraform"
   allow_access = true
@@ -20,7 +20,7 @@ resource "azuredevops_variable_group" "vars" {
 }
  
 resource "azuredevops_git_repository" "repo" {
-  project_id = azuredevops_project.p.id
+  project_id = azuredevops_project.a.id
   name       = "Code Repo"
   initialization {
     init_type = "Clean"
@@ -28,15 +28,21 @@ resource "azuredevops_git_repository" "repo" {
 }
  
 resource "azuredevops_build_definition" "build" {
-  project_id = azuredevops_project.p.id
+  project_id = azuredevops_project.a.id
   name       = "Inital Pipeline"
+  
  
   repository {
     repo_type   = "TfsGit"
-    repo_name   = azuredevops_git_repository.repo.name
+    repo_id     = azuredevops_git_repository.repo.id
     branch_name = azuredevops_git_repository.repo.default_branch
     yml_path    = "azure-pipelines.yml"
   }
  
   variable_groups = [azuredevops_variable_group.vars.id]
 }
+
+
+# Make sure to set the following environment variables:
+#   AZDO_PERSONAL_ACCESS_TOKEN
+#   AZDO_ORG_SERVICE_URL
